0706e3c53d65ad0378bacb2ba54bb38b1b4f6840
mariosknl <mariosknl@outlook.com>
2024-12-06 19:02:08 +0200

    creating the event details page

---
+++ src/app/dashboard/category/[name]/CategoryPageContent.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { EventCategory } from "@prisma/client"
+import { useQuery } from "@tanstack/react-query"
+import { EmptyCategoryState } from "./EmptyCategoryState"
+
+interface CategoryPageContentProps {
+  hasEvents: boolean
+  category: EventCategory
+}
+
+export const CategoryPageContent = ({
+  hasEvents: initialHasEvents,
+  category,
+}: CategoryPageContentProps) => {
+  const { data: pollingData } = useQuery({
+    queryKey: ["category", category.name, "hasEvents"],
+    initialData: { hasEvents: initialHasEvents },
+  })
+
+  if (!pollingData.hasEvents) {
+    return <EmptyCategoryState categoryName={category.name} />
+  }
+
+  return <div>CategoryPageContent</div>
+}
