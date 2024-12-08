277110703975e0fd2b8a664010989d50b0b50c6a
mariosknl <mariosknl@outlook.com>
2024-12-08 10:03:47 +0200

    discord integration for real-time message implemented

--- src/app/api/v1/events/route.ts
+++ src/app/api/v1/events/route.ts
@@ -1,5 +1,6 @@
 import { FREE_QUOTA, PRO_QUOTA } from "@/config"
 import { db } from "@/db"
+import { DiscordClient } from "@/lib/discord-client"
 import { CATEGORY_NAME_VALIDATOR } from "@/lib/validators/category-validator"
 import { NextRequest, NextResponse } from "next/server"
 import { z } from "zod"
@@ -78,4 +79,106 @@ export const POST = async (req: NextRequest) => {
       { status: 429 }
     )
   }
+
+  const discord = new DiscordClient(process.env.DISCORD_BOT_TOKEN)
+
+  const dmChannel = await discord.createDM(user.discordId)
+
+  let requestData: unknown
+
+  try {
+    requestData = await req.json()
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Invalid JSON request body",
+      },
+      { status: 400 }
+    )
+  }
+
+  const validationResult = REQUEST_VALIDATOR.parse(requestData)
+
+  const category = user.EventCategories.find(
+    (cat) => cat.name === validationResult.category
+  )
+
+  if (!category) {
+    return NextResponse.json(
+      {
+        message: `You don't have a category named "${validationResult.category}"`,
+      },
+      { status: 404 }
+    )
+  }
+
+  const eventData = {
+    title: `${category.emoji || "🔔"} ${
+      category.name.charAt(0).toUpperCase() + category.name.slice(1)
+    }`,
+    description:
+      validationResult.description ||
+      `A new ${category.name} event has occured!`,
+    color: category.color,
+    timesttamp: new Date().toISOString(),
+    fields: Object.entries(validationResult.fields || {}).map(
+      ([key, value]) => {
+        return {
+          name: key,
+          value: String(value),
+          inline: true,
+        }
+      }
+    ),
+  }
+
+  const event = await db.event.create({
+    data: {
+      name: category.name,
+      formattedMessage: `${eventData.title}\n\n${eventData.description}`,
+      userId: user.id,
+      fields: validationResult.fields || {},
+      eventCategoryId: category.id,
+    },
+  })
+
+  try {
+    await discord.sendEmbed(dmChannel.id, eventData)
+
+    await db.event.update({
+      where: { id: event.id },
+      data: { deliveryStatus: "DELIVERED" },
+    })
+
+    await db.quota.upsert({
+      where: { userId: user.id, month: currentMonth, year: currentYear },
+      update: { count: { increment: 1 } },
+      create: {
+        userId: user.id,
+        month: currentMonth,
+        year: currentYear,
+        count: 1,
+      },
+    })
+  } catch (err) {
+    await db.event.update({
+      where: { id: event.id },
+      data: { deliveryStatus: "FAILED" },
+    })
+
+    console.log(err)
+
+    return NextResponse.json(
+      {
+        message: "Error processing event",
+        eventId: event.id,
+      },
+      { status: 500 }
+    )
+  }
+
+  return NextResponse.json({
+    message: "Event processed successfully",
+    eventId: event.id,
+  })
 }
